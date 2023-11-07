2de3a8971ca9e17d9645de87686ea38161e97190
Anthony Obiako <anthonyobiakoc@gmail.com>
2023-11-07 01:05:03 +0100

    done with the feedback component

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -130,27 +130,19 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
-  },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
+      "Working with Obiako was a very delightful experience as he exceeded my expectations on every project.",
+    name: "Jude Sunday",
+    designation: "CEO",
+    company: "Tony Jp",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
+      "He is my first encounter with a web developer who truly cares about their client's success like Obiako does.",
+    name: "John",
     designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    company: "Viclyd",
+    image: "https://randomuser.me/api/portraits/men/13.jpg",
   },
 ];
 
